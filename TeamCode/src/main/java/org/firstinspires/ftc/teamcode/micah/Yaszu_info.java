02c61f43a4b170c9594e69d85efa7d65a5ed297b
Yaszu <sunguildstudios@gmail.com>
2024-11-02 08:30:07 -0700

    updated for comp using servo test for upbringer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/micah/Yaszu_info.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/micah/Yaszu_info.java
@@ -16,8 +16,8 @@ public class Yaszu_info extends LinearOpMode {
     // Declare Different Motors, Senses and other Variables
     //Unimplemented Lines in Build - Line(s) 18, 32, 95-100, 34, 19
     // Tinder^2 work im doing it I swear
-    private Servo Tinder;
-    private Servo Upbringer;
+    //private Servo Tinder;
+    //private Servo Upbringer;
     private final int READ_PERIOD = 1;
     private DcMotor LeftMotor;
     private DcMotor RightMotor;
@@ -32,8 +32,8 @@ public class Yaszu_info extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         //Actually Connecting Left Drive and Right Drive to correct Movement Class
-        Tinder = hardwareMap.get(Servo.class, "tinder");
-        Upbringer = hardwareMap.get(Servo.class, "Upbringer");
+        //Tinder = hardwareMap.get(Servo.class, "tinder");
+        //Upbringer = hardwareMap.get(Servo.class, "Upbringer");
         LeftMotor  = hardwareMap.get(DcMotor.class, "LeftDrive");
         RightMotor = hardwareMap.get(DcMotor.class, "RightDrive");
         servotest = hardwareMap.get(Servo.class, "servotest");
@@ -68,7 +68,7 @@ public class Yaszu_info extends LinearOpMode {
                 RightMotor.setPower(1);
             }else if (gamepad1.dpad_down){
                 LeftMotor.setPower(1);
-                RightMotor.setPower(-1)
+                RightMotor.setPower(-1);
             }else if (Math.abs(gamepad1.right_stick_x) > 0 || Math.abs(gamepad1.right_stick_y) > 0) {
                 if (Math.abs(gamepad1.right_stick_x) > Math.abs(gamepad1.right_stick_y)){
                     telemetry.addLine("Moving Left");
@@ -83,25 +83,21 @@ public class Yaszu_info extends LinearOpMode {
                 LeftMotor.setPower(0);
                 RightMotor.setPower(0);
             }
-            if (gamepad1.a) {
-                // check a bool and do things accordingly
-                if (jiamin) {
-                    // toggle on
-                    servotest.setPosition(0.0);
-                } else {
-                    servotest.setPosition(60.0);
-                    //toggle off
+                if (gamepad1.right_bumper || gamepad1.right_trigger > 0) {
+                    //differentiate
+                    if (gamepad1.right_bumper) {
+                        servotest.setPosition(servotest.getPosition() + 10);
+                    }
+                    if (gamepad1.right_trigger > 0) {
+                        servotest.setPosition(servotest.getPosition() - 10);
+                    }
                 }
-                jiamin = !jiamin;
-                Cool.reset();
-                //flip bool
-            }
-            if (gamepad1.left_bumper || gamepad1.left_trigger) {
-                if (gamepad1.left_bumper) {
-                    Upbringer.setPosition(Upbringer.getPosition() + 180);
-                }else if (gamepad1.left_trigger) {
-                    Upbringer.setPosition(Upbringer.getPosition() - 180);
-                }
+          //  if (gamepad1.left_bumper || gamepad1.left_trigger > 0) {
+            //    if (gamepad1.left_bumper) {
+              //      Upbringer.setPosition(Upbringer.getPosition() + 180);
+                //}else if (gamepad1.left_trigger > 0) {
+                //    Upbringer.setPosition(Upbringer.getPosition() - 180);
+                //}
             }
             if (gamepad1.a) {
                 if (aidan) {
